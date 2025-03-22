5dd00b65a7b75048acef2538864e14f42d296f33
Imenda-tl5 <Imendat.com>
2025-03-22 02:34:29 +0200

    here we go

---
+++ FrontEnd/vite-project/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from 'react'
+import { FoodContext } from './COMPONENTS/FoodContext'
+import About from './COMPONENTS/pages/About/About'
+import Menu from './COMPONENTS/pages/Menu/Menu'
+import Navbar from './COMPONENTS/Navbar/Navbar'
+import { Routes,Route } from 'react-router-dom'
+import Home from './COMPONENTS/pages/Home'
+import Cart from './COMPONENTS/cart/Cart'
+import LoginPopUp from './COMPONENTS/LoginPopUp/loginPopUp'
+
+const App = () => {
+  const {login} = useContext(FoodContext)
+  return (
+    <div>
+<Navbar/>
+{
+  login==="not_logged_in"?<></>:
+<LoginPopUp/>
+
+}
+<Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ FrontEnd/vite-project/src/COMPONENTS/LoginPopUp/loginPopUp.jsx
@@ -0,0 +1,32 @@
+import React, { useContext, useState } from 'react'
+import "./loginPopUp.css"
+import { FoodContext } from '../FoodContext'
+const LoginPopUp = () => {
+  const {login,setLogin} = useContext(FoodContext)
+  return (
+    <div className='login'>
+        <div className="login-container">
+            <div className="header">
+
+              <div className="log-in-head">
+              <h1>{login==="signUp"?"SIGNUP":"LOGIN"}</h1> <h1 onClick={()=>setLogin("not_logged_in")} className='close'>X</h1>
+              </div>
+            </div>
+            <div className="">
+              <div className="input-fields">
+                {login==="signUp"?<input type="text" id="name" placeholder='Your Name'/>:<></>}
+                <input type="email" id='email'  placeholder='Your Email'/>
+                <input type="password" id='password' placeholder='Password'/>
+              </div>
+            </div>
+            <div className="info">
+                <p>{login==="signUp"?"already  have an account":"dont have an account yet"}? Click here to <span onClick={()=>setLogin(prev=>prev==="logIn"?"signUp":"logIn")}>{login==="signUp"? "logIn":"signUp"}</span> </p>
+                <button>{login==="signUp"?"create Account":"Login"}</button>   
+
+            </div>
+            </div>
+    </div>
+  )
+}
+
+export default LoginPopUp
